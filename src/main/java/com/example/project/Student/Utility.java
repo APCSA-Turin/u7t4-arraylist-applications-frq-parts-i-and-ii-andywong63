49b9f753656b9a09c1471d96f9b84dcb35473155
Andy <andyw63@nycstudents.net>
2025-02-11 16:53:41 +0000

    Part 2

--- src/main/java/com/example/project/Student/Utility.java
+++ src/main/java/com/example/project/Student/Utility.java
@@ -4,8 +4,49 @@ import java.util.Arrays;
 
 public class Utility {
 
-    public static ArrayList<Student> sortStudents(ArrayList<Student> list){
+    public static ArrayList<Student> sortStudents(ArrayList<Student> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Student currentStudent = list.get(i);
+            for (int j = 0; j < i; j++) {
+                Student checkStudent = list.get(j);
+                int lastNameCompare = currentStudent.getLastName().compareTo(checkStudent.getLastName());
+                int firstNameCompare = currentStudent.getFirstName().compareTo(checkStudent.getFirstName());
+                boolean higherGPA = currentStudent.getGpa() > checkStudent.getGpa();
+                boolean move = false;
+                if (lastNameCompare < 0) {
+                    // Last name is before
+                    move = true;
+                } else if (lastNameCompare == 0 && firstNameCompare < 0) {
+                    // Last name is same but first name is before
+                    move = true;
+                } else if (lastNameCompare == 0 && firstNameCompare == 0 && higherGPA) {
+                    // Same full name but higher GPA
+                    move = true;
+                }
+                if (move) {
+                    list.remove(i);
+                    list.add(j, currentStudent);
+                    break;
+                }
+            }
+        }
         return list;
     }
 
+    public static void main(String[] args) {
+        Student s1 = new Student("Abby", "Smith", 96.7);
+        Student s2 = new Student("Michelle", "Jones", 98.1);
+        Student s3 = new Student("Chase", "Bean", 95.2);
+        Student s4 = new Student("Jack", "Clancy", 97.3);
+        Student s5 = new Student("Brittany", "Jones", 95.5);
+        Student s6 = new Student("Lisa", "Frank", 92.4);
+        Student s7 = new Student("Marie", "Angelina", 96.5);
+        Student s8 = new Student("Peter", "Frank", 97.9);
+        Student s9 = new Student("Lisa", "Frank", 99.5);
+        Student s10 = new Student("Dean", "Jones", 93.2);
+
+        ArrayList<Student> students = new ArrayList<Student>(Arrays.asList(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10));
+
+        System.out.println(sortStudents(students));
+    }
 }
--- src/main/java/com/example/project/WordPair/WordPairList.java
+++ src/main/java/com/example/project/WordPair/WordPairList.java
@@ -6,7 +6,12 @@ public class WordPairList {
     private ArrayList<WordPair> allPairs;
 
     public WordPairList(String[] words) {
-        /* to be implemented in part (a) */
+        allPairs = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                allPairs.add(new WordPair(words[i], words[j]));
+            }
+        }
     }
 
     // The method below was added for testing;
@@ -16,8 +21,13 @@ public class WordPairList {
     }
 
     public int numMatches() {
-        /* to be implemented in part (b) */
-        return 0;
+        int matches = 0;
+        for (WordPair pair : allPairs) {
+            if (pair.getFirst().equals(pair.getSecond())) {
+                matches++;
+            }
+        }
+        return matches;
     }
 
     // The method below was added for testing;
